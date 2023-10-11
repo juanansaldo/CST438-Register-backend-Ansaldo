1d14d514918fdbeab99ec311d58312f0a09bb4b1
Juan Ansaldo <131718774+juanansaldo@users.noreply.github.com>
2023-10-10 19:49:23 -0700

    Add files via upload

--- cst438_registration/src/main/resources/data.sql
+++ cst438_registration/src/main/resources/data.sql
@@ -11,7 +11,8 @@ INSERT INTO course VALUES
 (2020,'Fall',31249,1,'CST 237 - Intro to Computer Architecture','TuTh 2:00PM - 3:50PM','506','104','sislam@csumb.edu','2020-08-24','2020-12-13'),
 (2020,'Fall',31253,1,'BUS 307 - Finance','We 2:00PM - 3:50PM','506','112','hwieland@csumb.edu','2020-08-24','2020-12-13'),
 (2020,'Fall',31747,1,'CST 238 - Introduction to Data Structures','Mo 2:00PM - 2:50PM','506','117','jgross@csumb.edu','2020-08-24','2020-12-13'),
-(2021,'Fall',40442,1,'CST 363 - Introduction to Database Systems','MoWe 4:00PM - 5:50PM','506','104','dwisneski@csumb.edu', '2021-08-24', '201-12-13')
+(2021,'Fall',40442,1,'CST 363 - Introduction to Database Systems','MoWe 4:00PM - 5:50PM','506','104','dwisneski@csumb.edu', '2021-08-24', '2021-12-13'),
+(2021,'Fall',40443,1,'CST 463 - Advanced Machine Learning','TuTh 12:00PM - 1:50PM','506','117','dwisneski@csumb.edu', '2021-08-24', '2021-12-13')
 ;
 
 insert into enrollment values 
@@ -19,4 +20,5 @@ insert into enrollment values
 (2, 1, 2020, 'Fall', 30163, null),
 (3, 1, 2020, 'Fall', 31045, null),
 (4, 2, 2020, 'Fall', 31045, null),
-(5, 3, 2020, 'Fall', 31045, null);
+(5, 3, 2020, 'Fall', 31045, null),
+(6, 3, 2021, 'Fall', 40443, null);
